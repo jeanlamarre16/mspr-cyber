e543b198ec8e429733167832392a1634ceadfb44
Lamarre <jeanlamarre16@yahoo.fr>
2023-03-09 15:43:39 +0100

    commit de test

---
+++ CRM_ACME/src/main/java/fr/acme/CRM_ACME/CrmAcmeApplication.java
@@ -0,0 +1,13 @@
+package fr.acme.CRM_ACME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAcmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmAcmeApplication.class, args);
+	}
+
+}
---
+++ CRM_ACME/src/test/java/fr/acme/CRM_ACME/CrmAcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.acme.CRM_ACME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAcmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
